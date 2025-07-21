45a805434ce5575136dd41455ee35fff27932f73
guilhermeleeno <guilhermelino@msn.com>
2025-07-21 16:02:27 -0300

    xadrez.c
    aventureiro

--- xadrez.c
+++ xadrez.c
@@ -29,8 +29,41 @@ int main() {
         printf("Esquerda\n");
         rainha++;
     }
+    //Segunda parte do desafio - Aventureiro
+
+    printf("\nMovimento do cavalo - 1 (for)\n");
+
+    //Utilizando o loop for, sinalizei para saber qual etapa do loop a palavra baixo foi impressa.
     
+    for (int cavalo1 = 0; cavalo1 <1 ; cavalo1++)
+    {
+        for (int cavalo2 = 0; cavalo2 <1 ; cavalo2++)
+            printf("Baixo-\n");
+    {
+        printf("Baixo*\n"); //sinalizei para ver o loop *
+    }
+    
+        printf("Esquerda\n");
+    }
+    
+    printf("\nMovimento do cavalo 2 - (while)");
+    
+        int cavalo1 =0, cavalo2 =0;
 
-
+    //Loop while tambem sinalizado para observar a palavra 'baixo' impressa.
+    
+    while (cavalo1 < 1){
+        while(cavalo2 < 1){
+            
+            printf("\nBaixo-");
+            cavalo2 ++;
+        }
+        
+        printf("\nBaixo*");
+        cavalo1++;
+    }
+    printf("\nEsquerda");
     return 0;
 }
+
+
